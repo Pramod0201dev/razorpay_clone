5683891ca03234c527603f91aecbac4ce58e2390
Pramod Singh Rawat <119959029+Pramod0201dev@users.noreply.github.com>
2023-08-07 19:04:53 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontfamily: {
+      mullish: ["mulish", "sans-sarif"],
+    },
+      colors: {
+        deepBlue: "#02042a",
+        lightBlue: "#2b84ea",
+        lightBlue300: "#4b94ed",
+        lightblue500: "#0b72e7",
+        greenLight: "#61cea6",
+        grayText: "#818597",
+        lightGray: "#e2e2e2",
+        grayBlue: "#344a6c",
+        deepBlueHead: "#162f56",
+        gray2: "#525a76",
+      },
+    },
+  },
+  plugins: [],
+}
+
+  
+
